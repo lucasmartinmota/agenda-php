7623221707678db6e1f1b1cad823f9d088c05e56
Lucas Martin Mota <lucas127mm@gmail.com>
2023-05-26 16:20:24 -0300

    ngostodephp

--- pages/paginaInicialUser.php
+++ pages/paginaInicialUser.php
@@ -6,12 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agenda Menu</title>
     <link rel="stylesheet" href="../styles/principal.css">
-    <style>
-        
-        .color-1 { background-color: #FF2345; }
-        .color-2 { background-color: #01FF18; }
-        .color-3 { background-color: #f3f45f; }
-    </style>
 </head>
 <body>
     <div class="menu">
@@ -22,28 +16,5 @@
         </div>
     </div>
 
-    <script>
-        function getCurrentTime() {
-            return new Date().getTime();
-        }
-
-        
-        function checkLastAccess() {
-            var lastAccess = localStorage.getItem('lastAccess'); 
-
-            if (lastAccess && currentTime - lastAccess > 90000) {
-                var randomColor = Math.floor(Math.random() * 3) + 1;
-                var bodyElement = document.querySelector('body');
-
-                bodyElement.classList.remove('color-1', 'color-2', 'color-3');
-
-                bodyElement.classList.add('color-' + randomColor);
-            }
-
-            localStorage.setItem('lastAccess', currentTime);
-        }
-
-        window.onload = checkLastAccess;
-    </script>
 </body>
 </html>
